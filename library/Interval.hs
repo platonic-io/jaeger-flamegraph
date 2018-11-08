3b671c9d62981c326d1edd3e4b33d86ea32bad7a
Sam Halliday <sam.halliday@gmail.com>
2018-11-08 17:50:15 +0000

    initial public release

---
+++ library/Interval.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Interval
+  ( Interval
+  , interval
+  , measure
+  , width)
+where
+
+import           Data.List                 (sort)
+import           Test.QuickCheck.Arbitrary (Arbitrary, arbitrary)
+
+-- nothing on http://hackage.haskell.org/packages/search?terms=interval
+data Interval = Interval Integer Integer   -- ^ assume from <= to
+                deriving (Eq, Ord, Show)
+
+interval :: Integer -> Integer -> Interval
+interval a b | a <= b    = Interval a b
+             | otherwise = Interval b a
+
+width :: Interval -> Integer
+width (Interval from to) = to - from
+
+-- idea by etorreborre
+measure :: [Interval] -> Integer
+measure unsorted =
+  count $ sieve sorted
+  where
+    sorted = sort unsorted
+    sieve (car@(Interval a b) : cadr@(Interval c d) : rest) =
+      if a == c || c <= b
+      then sieve $ Interval a (max b d) : rest
+      else car : sieve (cadr : rest)
+    sieve done = done
+    count is = sum $ width <$> is
+
+instance Arbitrary Interval where
+  arbitrary = do a <- arbitrary
+                 b <- arbitrary
+                 pure $ interval a b
---
+++ test/IntervalTest.hs
@@ -0,0 +1,32 @@
+module IntervalTest where
+
+import           Data.List        (nub, permutations)
+import           Interval
+import           Test.Tasty.Hspec
+
+-- https://hspec.github.io/writing-specs.html
+-- https://hackage.haskell.org/package/tasty-discover
+
+spec_interval :: Spec
+spec_interval = do
+  describe "measure" $ do
+    it "should support one interval" $ do
+      (measure [interval 10 12]) `shouldBe` 2
+
+    it "should support disjoint intervals" $ do
+      (measure [(interval 10 12), (interval 20 25)]) `shouldBe` 7
+
+    it "should support a more complex interval" $ do
+      (measure [ (interval 10 12)
+               , (interval 20 25)
+               , (interval 10 25)
+               , (interval 21 22)
+               ]) `shouldBe` 15
+
+prop_lte :: [Interval] -> Bool
+prop_lte is = (measure is) <= (sum $ width <$> is)
+
+-- permutations is too slow, so trim the list
+prop_permutations :: [Interval] -> Bool
+prop_permutations is =
+  1 == (length $ nub $ measure <$> (permutations . take 7 $ is))
